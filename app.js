899d8fb57bd139351af5e4b18c46302247186c84
Dina <dinagiacobbi@gmail.com>
2023-03-19 10:18:54 -0800

    Adding hide image feature

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const colorModeBtn = document.getElementById('btn')
 
-colorModeBtn.addEventListener('click', function(){
-    let element = document.body;
-    element.classList.toggle("dark-mode")
+colorModeBtn.addEventListener('click', function () {
+  let element = document.body
+  element.classList.toggle('dark-mode')
 })
 
-const greet = document.getElementById('name')
-greet.addEventListener ('mouseover' , function (){
-    greet.style.color = "brown"
+const myName = document.getElementById('name')
+myName.addEventListener('mouseover', function () {
+  myName.style.color = 'brown'
+})
+
+const hideButton = document.getElementById('hide_btn')
+const avatar = document.querySelector('.prof-img')
+
+// hideButton.addEventListener('click', () => {
+//   removeHiddenClasses()
+//   avatar.classList.add('hide-me')
+// })
+
+// function removeHiddenClasses() {
+//   avatar.classList.remove('hide-me')
+// }
+
+hideButton.addEventListener('click', ()=> {
+    avatar.classList.toggle('hide-me')
 })
